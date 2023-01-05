fa016b0c0be345ad29e6663ee6295abbfd5f569c
Guillaume <levasseurguillaume91@gmail.com>
2023-01-05 15:54:18 +0100

    DONE ex Calc

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const main = require("./30_myObjectValues")
+const main = require("./31_myCalc")
 
-console.log(main({surname: "fzef", name: "FEZEF", age: 25, method: () => {return "test";}, obj: [Object], arr: [1,2,3]})); 
-arr = [1,2,3]
-console.log(typeof(arr))
+
+let calc = new Calc(); 
+ 
+console.log(calc.compute("1+2")); //"1+2=3" 
+console.log(calc.compute("2-1")); //"2-1=1" 
+console.log(calc.compute("2*3")); //"2*3=6" 
+console.log(calc.compute("10/2")); //"10/2=5" 
+ 
+console.log(calc.lastResult()); //5 
+console.log(calc.history());//["1+2=3","2-1=1","2*3=6","10/2=5"]
